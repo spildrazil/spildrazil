15ecfcf14d351259a69538ac3e1c549f8fc2b891
johnsonr <johnsonroda@gmail.com>
2017-08-09 10:36:23 -0700

    Update Controller.java

--- src/main/java/com/atomist/t1/Controller.java
+++ src/main/java/com/atomist/t1/Controller.java
@@ -11,6 +11,10 @@ public class HomeController {
         return "Hello REST Microservice World";
     }
     
+     @RequestMapping(path = "/3")
+    public String home3() {
+        return "Hello REST Microservice World";
+    }
     
  
 }
